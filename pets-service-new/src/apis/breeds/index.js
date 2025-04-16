b300f3e90e840faa45fea6c894002a4962a633ba
tchhetri0995 <taanvi.chhetri@gmail.com>
2025-04-16 13:35:17 +0530

    Delete pets-service-new directory
    Deleting this as it's the old file structure

--- pets-service-new/src/apis/breeds/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/:id', require('./getBreed'));
-router.post('/', require('./createBreed'));
-router.put('/:id', require('./updateBreed'));
-router.delete('/:id', require('./deleteBreed'));
-
-module.exports = router;
--- pets-service-new/src/apis/pets/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/:id', require('./getPet'));
-router.post('/', require('./createPet'));
-router.put('/:id', require('./updatePet'));
-router.delete('/:id', require('./deletePet'));
-
-module.exports = router;
